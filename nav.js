6c0c62f103fedb14451b001e85fbdabe73df4116
kylead10 <adiokayode111@gmail.com>
2023-12-06 22:05:56 +0100

    hover effect

--- nav.js
+++ nav.js
@@ -1,38 +1,36 @@
-const nav_links = document.querySelector('.nav-links');
-const menuBtn_open = document.querySelector('#menu-btn_open');
-const menuBtn_close = document.querySelector('#menu-btn_close');
-// console.log(nav_links, menuBtn_open, menuBtn_close);
+document.addEventListener('DOMContentLoaded', function () {
+  const nav_links = document.querySelector('.nav-links');
+  const menuBtn_open = document.querySelector('#menu-btn_open');
+  const menuBtn_close = document.querySelector('#menu-btn_close');
+  const cursor = document.querySelector('.cursor');
+  var timeout;
 
-menuBtn_open.onclick = () => toggler();
-menuBtn_close.onclick = () => toggler();
-function toggler() {
-  nav_links.classList.toggle('active');
-  menuBtn_open.classList.toggle('close');
-  menuBtn_close.classList.toggle('open');
-}
-// console.log('nav');
-const cursor = document.querySelector('.cursor');
-var timeout;
-//cursor movement
-document.addEventListener('mousemove', (e) => {
-  let x = e.pageX;
-  let y = e.pageY;
+  menuBtn_open.onclick = () => toggler();
+  menuBtn_close.onclick = () => toggler();
 
-  cursor.style.top = y + 'px';
-  cursor.style.left = x + 'px';
-  cursor.style.display = 'block';
-
-  //stop animation
-  function mouseStopped() {
-    cursor.style.display = 'none';
+  function toggler() {
+    nav_links.classList.toggle('active');
+    menuBtn_open.classList.toggle('close');
+    menuBtn_close.classList.toggle('open');
   }
 
-  //hide animation
-  clearTimeout(timeout);
-  timeout = setTimeout(mouseStopped, 1000);
-});
+  document.addEventListener('mousemove', (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
 
-//stop animation off screen
-document.addEventListener('mouseout', () => {
-  cursor.style.display = 'none';
+    cursor.style.top = y + 'px';
+    cursor.style.left = x + 'px';
+    cursor.style.display = 'block';
+
+    function mouseStopped() {
+      cursor.style.display = 'none';
+    }
+
+    clearTimeout(timeout);
+    timeout = setTimeout(mouseStopped, 1000);
+  });
+
+  document.addEventListener('mouseout', () => {
+    cursor.style.display = 'none';
+  });
 });
